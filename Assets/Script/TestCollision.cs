9efdf9419bf0aa6245402223b0c1de4da638c7dd
dmy0511 <dmy0511love@gmail.com>
2023-08-24 12:04:14 +0900

    1

---
+++ Assets/Script/TestCollision.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class TestCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log($"Collision @ {other.gameObject.name}");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100, Color.red, 1.0f);
+
+            LayerMask mask = LasyerMask.GetMask("Moster") | LasyerMask.GetMask("Wall");
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, mask))
+            {
+                Debug.Log($"Raycast Camera @{hit.collider.gameObject.name}");
+            }
+        }
+    }
+}
